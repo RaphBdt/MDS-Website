ca591a02d63b21bf058cd9ee8adeb57988b4335a
Raphaël Beaudet <raphaelbeaudet@icloud.com>
2023-02-27 11:15:16 +0100

    navbar done

---
+++ src/components/Nav.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+export default {
+    setup() {
+
+    }
+}
+</script>
+
+<template>
+    <nav>
+        <div class="container mx-auto">
+            <div class="flex justify-between items-center">
+                <a href="https://vuejs.org/" target="_blank">
+                    <img src="../assets/logo-mds.png" class="logo vue w-[131px]" alt="Logo MDS" />
+                </a>
+                <div>
+                    <a class="text-[#07052F] font-bold mx-3">Accueil</a>
+                    <a class="text-[#A5A5A5] font-bold mx-3">Les programmes</a>
+                    <a class="text-[#A5A5A5] font-bold mx-3">Notre blog</a>
+                </div>
+                <a class="bg-[#29B7C5] px-5 py-3 text-white rounded-full" href="">M'inscrire</a>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
