8569397843591f555edd1419ab2af2f852830db3
Raphaël Beaudet <raphaelbeaudet@icloud.com>
2023-02-27 11:26:30 +0100

    main title component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Nav from './components/Nav.vue'
+import MainTitle from './components/MainTitle.vue'
 </script>
 
 <template>
-  <Nav class="my-5" />
+  <Nav class="my-14" />
+  <MainTitle class="w-[600px] mx-auto mt-20" msg="La meilleure école des métiers du digital" />
 </template>
 
 <style scoped>
---
+++ src/components/MainTitle.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  msg?: string
+}>()
+</script>
+<script lang="ts">
+export default {
+    setup() {
+
+    }
+}
+</script>
+
+<template>
+    <h1 class="font-extrabold text-[50px] text-center leading-[60px]">{{ msg }}</h1>
+</template>
+
+<style scoped>
+
+</style>
