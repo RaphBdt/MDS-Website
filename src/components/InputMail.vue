94b5ffdf1a6d2ce7797d0fd494baa87add71db14
Raphaël Beaudet <raphaelbeaudet@icloud.com>
2023-02-28 13:57:11 +0100

    end of the integration

---
+++ src/components/InputMail.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Button from './Button.vue'
+
+
+export default defineComponent({
+    components: { Button }
+})
+</script>
+
+<template>
+    <form class="w-[550px] mx-auto">   
+        <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+            <input type="search" id="search" class="rounded-full mt-16 block w-full p-5 pl-10 text-md text-gray-900 border border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 border-full" placeholder="Entre ton adresse e-mail ici" required>
+            <Button class="absolute right-[10px] bottom-[8px]" text="Envoyer" />
+        </div>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -1,9 +1,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import OtherTitle from './OtherTitles.vue'
+import InputMail from './InputMail.vue'
 
 export default defineComponent({
-    components: { OtherTitle },
+    components: { OtherTitle, InputMail },
     props: {
         title: String,
         description: String
@@ -18,6 +19,7 @@ export default defineComponent({
     <div id="newsletter-section" class="w-full h-[700px] pt-16">
         <OtherTitle class="text-white text-center w-[600px] mx-auto" type="h2" msg="Entre ton e-mail pour recevoir le programme de l’école" />
         <p class="text-white text-[20px] text-center leading-[26px] mt-10 font-light">Nous t’enverrons le programme complet, et quelqu’un<br /> prendra contact avec toi pour savoir tes besoins</p>
+        <InputMail />
     </div>
 </template>
 
